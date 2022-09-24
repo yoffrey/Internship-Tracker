7bced96cd24dbac2963c1b813b5a67b5b0cdc025
yoffrey <76462775+yoffrey@users.noreply.github.com>
2022-09-24 09:49:04 -0700

    Knows if checkbox is checked from json

--- internship-tracker/src/components/Box.js
+++ internship-tracker/src/components/Box.js
@@ -7,30 +7,39 @@ function Box(boxInfo){
     var initialDate = '__________';
     var emptyDate = '__________';
     var savedDate = dataSet[1];
-
     const savedCheck = dataSet[0];
+
+    var setChecked = false
+    if (savedCheck == '1'){
+        setChecked = true;
+    }
 
     if (savedDate != '__________'){
         initialDate = savedDate;
-    }
+    };
     
-
     const [dateAppText, setAppText] = useState(initialDate);
+    const [checked, setBoxChecked] = useState(setChecked)
 
-    const handleAppChange = event =>{
+    const handleAppChange = event => {
         if (event.target.checked){
             setAppText(formatDate);
         }
         else{
             setAppText(emptyDate);
         }
+        setBoxChecked(!checked);
     };
 
-
     return(
-        
         <h4>
-            <td><input type="checkbox" onChange={handleAppChange}/> {dateAppText} </td>
+            <td>
+                <input type="checkbox" 
+                    onChange={handleAppChange}
+                    checked={checked}
+                /> 
+                {dateAppText} 
+            </td>
         </h4>
     );
 }
