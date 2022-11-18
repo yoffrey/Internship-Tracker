deb84c966f187421593be4959178f9797b2dd6aa
yoffrey <76462775+yoffrey@users.noreply.github.com>
2022-11-17 18:50:01 -0800

    Delete double files

--- internship-tracker/src/components/updateLink.js
+++
@@ -1,9 +0,0 @@
-import checkLocalStorage from './checkLocalStorage';
-
-function updateLink(index, link) {
-    var internshipData = checkLocalStorage()
-    internshipData[index]['link'] = link;
-    localStorage.setItem('myData', JSON.stringify(internshipData))
-}
-
-export default updateLink
